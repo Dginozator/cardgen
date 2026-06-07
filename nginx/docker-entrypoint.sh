2cf4e61abe83886cdd616bc83f4ad8c7cc194258
dginozator <viz@dginozator.com>
2026-06-07 21:57:40 +0300

    fix: remove wait-for-nuxt, let nginx start immediately with DNS resolver

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -4,19 +4,10 @@ set -eu
 : "${DIRECTUS_UPSTREAM:?DIRECTUS_UPSTREAM is required}"
 : "${N8N_UPSTREAM:?N8N_UPSTREAM is required}"
 
-# Wait for Nuxt app DNS and HTTP endpoint to be reachable before nginx starts.
-# This avoids boot-time race conditions in orchestrated environments.
-i=0
-until wget -q -T 2 -t 1 -O- "http://nuxt:3000/" >/dev/null 2>&1; do
-  i=$((i + 1))
-  echo "waiting for nuxt upstream (${i}/60): http://nuxt:3000/"
-  if [ "$i" -ge 60 ]; then
-    echo "nuxt upstream is not reachable after 60s: http://nuxt:3000/"
-    exit 1
-  fi
-  sleep 1
-done
-
-envsubst '${DIRECTUS_UPSTREAM} ${N8N_UPSTREAM}' \
+# Generate nginx config with envsubst (runs before default 20-envsubst-on-templates.sh
+# but we use our own script to control exactly which vars are substituted)
+envsubst '${DIRECTUS_UPSTREAM} ${N8N_UPSTREAM} ${WORKER_UPSTREAM}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
+
+echo "nginx config generated successfully"
